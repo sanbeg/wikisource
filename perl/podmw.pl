8d42abf15f8bbeb7490dbbc3e8461caa6aa3c6fb
sanbeg <sanbeg>
2007-09-21 19:22:39 +0000

    remove stuyff.

--- perl/podmw.pl
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/perl
-
-use FindBin;
-use lib "$FindBin::Bin/../perl";
-use PodMw;
-
-my $parser = PodMw->new;
-$parser->parse_from_filehandle(\*STDIN)  if (@ARGV == 0);
-for (@ARGV) { $parser->parse_from_file($_); }
