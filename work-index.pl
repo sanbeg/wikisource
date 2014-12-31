eb7b35f4b6474be7e32200d45cfc2421fd5a1dfa
Steve Sanbeg <steve.sanbeg@gmail.com>
2014-12-30 21:32:44 -0500

    fix works script

--- work-index.pl
+++ work-index.pl
@@ -8,31 +8,43 @@ use lib 'archive'; #for password file.
 use lib '../MediaWiki-EditFramework/lib';
 use MediaWiki::EditFramework;
 use passwd;
-use open ':utf8';
-use open ':std';
+# use open ':utf8';
+# use open ':std';
 
 my $be_anon;
 my $do_edit;
 
 #skip various disambig pages
 my %bl = (
-'Category:Mainspace disambiguation pages' => 1,
-'Category:Case disambiguation pages' => 1,
-'Category:Versions pages' => 1,
-'Category:Translations pages' => 1,
-);
+	  'Category:Mainspace disambiguation pages' => 1,
+	  'Category:Case disambiguation pages' => 1,
+	  'Category:Versions pages' => 1,
+	  'Category:Translations pages' => 1,
+	 );
 
-GetOptions ('do_edit' => \$do_edit ) or die;
+my ($username, $password);
+my $l1 = 'a';
+
+GetOptions (
+	    'letter=s' => \$l1,
+	    'edit!' => \$do_edit,
+	    'username=s'=>\$username, 'password=s'=>\$password,
+	   ) or die;
+
+die "Invalid letter: $l1" unless $l1 =~ m/^[a-z]$/;
 
 my $wiki = MediaWiki::EditFramework->new('en.wikisource.org');
 $be_anon = 1 unless $do_edit;
-$wiki->login($::username, $::password) unless $be_anon;
 
+$username //= $::username;
+$password //= $::password;
+$wiki->login($username, $password) unless $be_anon;
+
+#$wiki->write_prefix('User:Sanbeg/');
+#$wiki->{text_dump_dir} = 'tmp.d';
 
 my %page_cats;
 my @batch;
-
-
 
 sub get_cats {
   my $cats = $wiki->api->api({
@@ -45,22 +57,26 @@ sub get_cats {
   while ( my ($pid, $data ) = each %{ $cats->{query}{pages} } ) {
     my $bl=0;
     my @cl;
-    foreach my $c ( @{ $data->{categories} } ) {
-      push @cl, $c->{title};
-      ++ $bl if $bl{ $c->{title} } ; #count blacklisted cats
-    };
+
+    if (defined $data->{categories}) {
+      #warn "Got cats for $data->{title}:" . @{ $data->{categories} };
+      foreach my $c ( @{ $data->{categories} } ) {
+	push @cl, $c->{title};
+	++ $bl if $bl{ $c->{title} } ; #count blacklisted cats
+      };
+    } else {
+      warn "No cats for $data->{title}";
+    }
     $page_cats{ $data->{title} } = \@cl unless $bl;
   };
   @batch = ();
 };
 
-# my $text='';
-# open my($fh), '>', \$text;
-# my $stdout = select $fh;
+my $text = '<div style="clear:right; margin-bottom:.5em; float:right; padding:.5em 0 .8em 1.4em; background:none;">__TOC__</div>';
 
 foreach my $letter ( 'a' .. 'z' ) {
-  my $prefix = ucfirst "a$letter";
-  print "\n== $prefix ==\n\n";
+  my $prefix = ucfirst "$l1$letter";
+  $text .= "\n== $prefix ==\n\n";
 
   my $articles = $wiki->api->list({
     action => 'query',
@@ -74,22 +90,23 @@ foreach my $letter ( 'a' .. 'z' ) {
   foreach my $page ( @$articles ) {
     next if $page->{title} =~ m:/:; #skip sub pages
     push @batch, $page->{title};
-    get_cats if @batch == 10;
+    get_cats if @batch; #should allow batching, but seem to miss a lot of cats
   };
-  
+
   get_cats if @batch;
-  
+
   #while ( my ($page, $cats) = each %page_cats ) {
   foreach my $page ( sort keys %page_cats ) {
+    #$page = decode_utf8($page);
     my $cats = $page_cats{$page};
 
-    print "* [[$page]]";
+    $text .= "* {{works-title|[[$page]]}}";
     if (@$cats) {
-      print" (",
-	join(', ', map("[[:$_|]]",@$cats)),
+      $text .= " &mdash; (".
+	join(', ', map("[[:$_|]]",@$cats)).
 	")";
     };
-    print "\n";
+    $text .=  "\n";
   };
 
   %page_cats = ();
@@ -97,5 +114,11 @@ foreach my $letter ( 'a' .. 'z' ) {
 
 }
 
-# close $fh;
-# $wiki->get_page ('User:Sanbeg/a')->edit($text, 'Create index of works');
+open T, '>tmp';
+print T encode_utf8(decode_utf8($text));
+
+if ($do_edit) {
+  #close $fh;
+  $wiki->get_page ("Wikisource:Works-\u$l1")->edit($text, 'Create index of works');
+}
+
